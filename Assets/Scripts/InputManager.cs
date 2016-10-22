4aee986800cf95bded25e2ed52604979472f6cfe
aserv <alexs9@vt.edu>
2016-10-21 21:40:43 -0400

    Add player controler

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Not using this right now, maybe later
+public class InputManager {
+    private string horizontalName = "Horizontal";
+    private string verticalName = "Vertical";
+    private string redName = "Red";
+    private string yellowName = "Yellow";
+    private string greenName = "Green";
+    private string blueName = "Blue";
+    private float tapTheshold;
+    public InputManager(int playerId, float tapTheshold = 0.8f) {
+        horizontalName += playerId;
+        verticalName += playerId;
+        redName += playerId;
+        yellowName += playerId;
+        greenName += playerId;
+        blueName += playerId;
+    }
+
+    public float HorizontalVal { get { return Input.GetAxis(horizontalName); } }
+    public float VerticalVal { get { return Input.GetAxis(verticalName); } }
+
+    public bool RedDown { get { return Input.GetButtonDown(redName); } }
+    public bool RedUp { get { return Input.GetButtonUp(redName); } }
+    public bool YellowDown { get { return Input.GetButtonDown(yellowName); } }
+    public bool YellowUp { get { return Input.GetButtonUp(yellowName); } }
+    public bool GreenDown { get { return Input.GetButtonDown(greenName); } }
+    public bool GreenUp { get { return Input.GetButtonUp(greenName); } }
+    public bool BlueDown { get { return Input.GetButtonDown(blueName); } }
+    public bool BlueUp { get { return Input.GetButtonUp(blueName); } }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is the player controller class
+/// </summary>
+public class PlayerControler : MonoBehaviour {
+    public float RunSpeed;
+    public float RunAccel;
+    public float JumpSpeed;
+    public int MaxJumps;
+
+    private float raycastLength = 1.2f;
+    private float raycastWidth = 0.5f;
+    private bool isGrounded;
+    private int jumpCount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = Physics2D.Raycast((Vector2)transform.position + new Vector2(raycastWidth, 0), Vector2.down, raycastLength, -1 ^ (1<<8)) ||
+            Physics2D.Raycast((Vector2)transform.position + new Vector2(-raycastWidth, 0), Vector2.down, raycastLength, -1 ^ (1 << 8));
+        Vector2 v = this.GetComponent<Rigidbody2D>().velocity;
+        v.x = RunSpeed * Input.GetAxis("Horizontal1");
+        //float v = Input.GetAxis("Vertical1");
+
+        jumpCount = isGrounded ? 0 : jumpCount;
+        if (jumpCount < MaxJumps && Input.GetButtonDown("Vertical1")) {
+            ++jumpCount;
+            v.y = JumpSpeed;
+        }
+
+        this.GetComponent<Rigidbody2D>().velocity = v;
+	}
+}
