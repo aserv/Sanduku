2907bb7a577fae5f5dd1f719fbb885658448e656
Daniel Amick <dka271@vt.edu>
2016-10-22 14:08:10 -0400

    Implement menus, testing for video textures

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour {
+	public void ButtonPressed(string newScene) {
+		SceneManager.LoadScene(newScene);
+	}
+	
+	public void QuitGamePressed() {
+		Application.Quit();
+	}
+	
+}
