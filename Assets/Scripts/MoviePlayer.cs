4be00e589d5c61159fde9f10e3df23ef5a2b54e6
Daniel Amick <dka271@vt.edu>
2016-10-22 19:00:49 -0400

    Adding animation to the menus

--- Assets/Scripts/MoviePlayer.cs
+++ Assets/Scripts/MoviePlayer.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoviePlayer : MonoBehaviour {
 
+	public MovieTexture movie;
+
 	// Use this for initialization
 	void Start () {
-		MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		//MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		GetComponent<RawImage>().texture = movie as MovieTexture;
+		movie.loop = true;
 		movie.Play();
 	}
 	
